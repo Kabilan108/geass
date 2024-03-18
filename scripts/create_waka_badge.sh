fa58777c3a528b68061b45fdfdeb79b6ca37a1e3
Tony Okeke <tonykabilanokeke@gmail.com>
2024-03-18 03:11:54 -0400

    docs: add badges

---
+++ scripts/create_waka_badge.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+current_dir=$(basename $(pwd))
+
+if [ $current_dir != "geass" ]; then
+    echo "This script must be run from the root of the geass project"
+    exit 1
+fi
+
+echo "[INFO] Fetching coding time from wakapi.dev"
+total_seconds=$(
+    curl -s -X GET -H "Authorization: Basic $(echo -n $WAKAPI_API_KEY | base64)" \
+    "https://wakapi.dev/api/summary?interval=all_time&project=geass" | \
+    jq '.projects[0].total')
+
+hours=$((total_seconds / 3600))
+minutes=$((total_seconds % 3600 / 60))
+
+formatted_time=$(printf "%d hrs %02d mins" $hours $minutes | sed 's/ /%20/g')
+
+readme_contents=$(cat README.md)
+
+modified_contents=$(echo "$readme_contents" | \
+    sed "s/\([0-9]\+\%20hrs\%20[0-9]\+\%20mins\)/$formatted_time/")
+
+echo "[INFO] Updating README.md with new coding time"
+echo "$modified_contents" > README.md
