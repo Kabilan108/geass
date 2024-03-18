0fdcfd6ad80470a2dad65dd66c9721158921f1a0
Tony Okeke <tonykabilanokeke@gmail.com>
2024-03-18 03:27:51 -0400

    feat: check if wakapi api key is available

--- scripts/create_waka_badge.sh
+++ scripts/create_waka_badge.sh
@@ -7,6 +7,11 @@ if [ $current_dir != "geass" ]; then
     exit 1
 fi
 
+if [ -z $WAKAPI_API_KEY ]; then
+    echo "WAKAPI_API_KEY is not set"
+    exit 1
+fi
+
 echo "[INFO] Fetching coding time from wakapi.dev"
 total_seconds=$(
     curl -s -X GET -H "Authorization: Basic $(echo -n $WAKAPI_API_KEY | base64)" \
